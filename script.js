fe1465f4472a4b1e894b84162741da57fe993d67
madhavi-neupane <neupanem206@gmail.com>
2025-09-19 23:24:54 +0545

    api added

---
+++ script.js
@@ -0,0 +1,6 @@
+function fetchWeather() {
+  let searchInput = document.getElementById('search-text').value
+  const weatherData = document.getElementById('weather-data')
+  weatherData.style.display = 'block'
+  const apiKey = '2f9aa47cc7dc75608d5a2249d1a09c1f'
+}
